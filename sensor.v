75c6b8b85206f4627df556e3404c2474e58cba8a
boop477 <wennielin1996@gamil.com>
2018-01-09 21:48:34 +0800

    trim code

--- sensor.v
+++ sensor.v
@@ -35,14 +35,13 @@ module Sensor(CLK, RESET_n, echo, trig, dist, out_get_echo);
                 dummy_dist <= dummy_dist + 1;
                 get_echo <= 1'b0;
             end
-            else if(counter == 32'd110) begin//1000
+            else if(counter == 32'd110) begin
                 // turn trig on for 10e5 seccond
                 r_trig <= 1'b0;
                 counter <= counter + 1;
-                //dist_counter <= 32'd0;
                 get_echo <= get_echo;
             end
-            else if(counter == 32'd1000000) begin//d1000000
+            else if(counter == 32'd1000000) begin
                 // 1 echo/sec
                 counter <= 32'd0;	
                 //dist_counter <= 32'd0;
@@ -51,7 +50,6 @@ module Sensor(CLK, RESET_n, echo, trig, dist, out_get_echo);
             end
             else begin
                 counter <= counter + 1'b1;
-                //dist_counter <= dist_counter + 1;
                 r_trig <= r_trig;
                 get_echo <= get_echo;
             end
@@ -64,7 +62,6 @@ module Sensor(CLK, RESET_n, echo, trig, dist, out_get_echo);
                 // Get echo!
                 dist_counter <= dist_counter;
                 if(get_echo == 1'b0) begin
-                    //r_dist <= (170000 * dist_counter) / 510000;
                     r_dist <= dist_counter / 100;//50000
                     get_echo <= 1'b1;
                 end
